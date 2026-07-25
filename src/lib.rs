15ed297ebc6910f3aebc4f263c10badd7524430b
Alex Fedin <alex_fedin@hotmail.com>
2026-07-25 14:53:49 -0700

    Publish hydra safe-uplift Rust mirror @ clang2rust 0.10.0-3299-g9fb6b28f

--- src/lib.rs
+++ src/lib.rs
@@ -45,40 +45,23 @@ pub(crate) struct SFILE {
 pub(crate) type FILE = SFILE;
 
 extern "C" {
-    fn __transpiler_isa(child: i32, ancestor: i32)
-    -> bool;
-    fn calloc(__count: u64, __size: u64)
-    -> *mut ();
-    fn fprintf(_: *mut FILE, _: *const i8, ...)
-    -> i32;
-    fn ioctl(_: i32, _: u64, ...)
-    -> i32;
-    fn strlen(__s: *const i8)
-    -> u64;
-    fn tcgetattr(_: i32, _: *mut Termios)
-    -> i32;
-    fn perror(_: *const i8)
-    -> ();
-    fn tcsetattr(_: i32, _: i32, _: *const Termios)
-    -> i32;
-    fn read(_: i32, _: *mut (), __nbyte: u64)
-    -> i64;
-    fn fopen(__filename: *const i8, __mode: *const i8)
-    -> *mut FILE;
-    fn exit(_: i32)
-    -> ();
-    fn fseek(_: *mut FILE, _: i64, _: i32)
-    -> i32;
-    fn ftell(_: *mut FILE)
-    -> i64;
-    fn rewind(_: *mut FILE)
-    -> ();
-    fn fread(__ptr: *mut (), __size: u64, __nitems: u64, __stream: *mut FILE)
-    -> u64;
-    fn fclose(_: *mut FILE)
-    -> i32;
-    fn setbuf(_: *mut FILE, _: *mut i8)
-    -> ();
+    fn __transpiler_isa(child: i32, ancestor: i32) -> bool;
+    fn calloc(__count: u64, __size: u64) -> *mut ();
+    fn fprintf(_: *mut FILE, _: *const i8, ...) -> i32;
+    fn ioctl(_: i32, _: u64, ...) -> i32;
+    fn strlen(__s: *const i8) -> u64;
+    fn tcgetattr(_: i32, _: *mut Termios) -> i32;
+    fn perror(_: *const i8) -> ();
+    fn tcsetattr(_: i32, _: i32, _: *const Termios) -> i32;
+    fn read(_: i32, _: *mut (), __nbyte: u64) -> i64;
+    fn fopen(__filename: *const i8, __mode: *const i8) -> *mut FILE;
+    fn exit(_: i32) -> ();
+    fn fseek(_: *mut FILE, _: i64, _: i32) -> i32;
+    fn ftell(_: *mut FILE) -> i64;
+    fn rewind(_: *mut FILE) -> ();
+    fn fread(__ptr: *mut (), __size: u64, __nitems: u64, __stream: *mut FILE) -> u64;
+    fn fclose(_: *mut FILE) -> i32;
+    fn setbuf(_: *mut FILE, _: *mut i8) -> ();
     static mut __stdoutp: *mut FILE;
     static mut __stderrp: *mut FILE;
 }
